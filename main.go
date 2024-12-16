fecfbe4338b84679c39cec56f55791724bc1e56b
Azat <azbyepublic@gmail.com>
2024-12-16 11:03:43 +0500

    mongodb

--- main.go
+++ main.go
@@ -1,15 +1,27 @@
 package main
 
 import (
+	"context"
 	"encoding/json"
 	"fmt"
 	"io"
 	"log"
 	"net/http"
 	"os"
+	"time"
 
 	"github.com/joho/godotenv"
+	"go.mongodb.org/mongo-driver/bson"
+	"go.mongodb.org/mongo-driver/mongo"
+	"go.mongodb.org/mongo-driver/mongo/options"
 )
+
+type WeatherData struct {
+	City        string    `bson:"city" json:"name"`
+	Description string    `bson:"description" json:"description"`
+	Temp        float64   `bson:"temp" json:"temp"`
+	LastUpdated time.Time `bson:"last_updated"`
+}
 
 type weatherjson struct {
 	Weather []struct {
@@ -24,7 +36,6 @@ type weatherjson struct {
 }
 
 func main() {
-	//loadin
 	err := godotenv.Load()
 	if err != nil {
 		log.Fatal("error loading .env file")
@@ -32,6 +43,7 @@ func main() {
 
 	BASE_URL := os.Getenv("BASE_URL")
 	API_KEY := os.Getenv("API_KEY")
+	MONGO_URI := os.Getenv("MONGO_URI")
 
 	fmt.Println("Where you want to check the weather:")
 
@@ -48,11 +60,53 @@ func main() {
 
 	defer response.Body.Close()
 
-	if response.StatusCode == http.StatusOK {
-		weatherBytes, _ := io.ReadAll(response.Body)
-		weather := weatherjson{}
-		json.Unmarshal(weatherBytes, &weather)
+	//context settings
 
-		fmt.Printf("Today the weather in %v is %v, temperature is %2.2v.", city, weather.Weather[0].Description, weather.Main.Temp-273.15)
+	ctx, cancel := context.WithTimeout(context.Background(), 10*time.Second)
+	defer cancel()
+
+	client, err := mongo.Connect(ctx, options.Client().ApplyURI(MONGO_URI))
+	if err != nil {
+		log.Fatal("Failed to create MONGODB Client:", err)
 	}
+
+	defer func() {
+		if err = client.Disconnect(ctx); err != nil {
+			log.Fatal("Failed to disconnect MONGODB:", err)
+		}
+	}()
+
+	err = client.Ping(ctx, nil)
+	if err != nil {
+		log.Fatal("Failed to connect to MongoDB:", err)
+	}
+
+	weatherCollection := client.Database("weatherdb").Collection("weather")
+
+	weatherBytes, _ := io.ReadAll(response.Body)
+	weather := weatherjson{}
+	json.Unmarshal(weatherBytes, &weather)
+
+	//preparing the data for mongodb
+
+	weatherData := WeatherData{
+		City:        weather.Name,
+		Description: weather.Weather[0].Description,
+		Temp:        weather.Main.Temp - 273.15,
+		LastUpdated: time.Now(),
+	}
+
+	// upsert data into MONGODB
+
+	filter := bson.M{"city": weatherData.City}
+	update := bson.M{"$set": weatherData}
+	opts := options.Update().SetUpsert(true)
+
+	_, err = weatherCollection.UpdateOne(ctx, filter, update, opts)
+	if err != nil {
+		log.Fatal("failed to update data:", nil)
+	}
+
+	fmt.Printf("Today the weather in %v is %v, temperature is %2.2v.", city, weather.Weather[0].Description, weather.Main.Temp-273.15)
+
 }
